78a455905062203dc1d0a0741f2fe5fc5dc76dae
Irving <Irv1ngzhu@hotmail.com>
2021-09-26 17:23:50 -0400

    added review entry

--- sprint-1/sprint1.py
+++ sprint-1/sprint1.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
+app = Flask(name)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 db = SQLAlchemy(app)
 
@@ -14,7 +14,7 @@ class User(db.Model):
     # balance of the user
     balance = db.Column(db.Integer, unique=False, nullable=True)
 
-    def __repr__(self):
+    def repr(self):
         return '<User %r>' % self.username
 
 
@@ -25,16 +25,30 @@ class Product(db.Model):
     product_price = db.Column(db.Integer, nullable=False)
     product_seller_id = db.Column(db.Integer, unique=False, nullable=True)
 
-    def __repr__(self):
+    def repr(self):
         return "<Product %r>" % self.product_id
 
+
 # class for shipping after transaction
-class  shipping(db.Model):
+class shipping(db.Model):
     shipping_id = db.Column(db.Integer, primary_key=True)
     shipping_contact = db.Column(db.Integer, primary_key=True)
     shipping_company = db.Column(db.String(80), unique=False, nullable=False)
-    shipping_sender = product_name = db.Column(db.String(80), unique=False, nullable=False)
-    shipping_receiver = product_name = db.Column(db.String(80), unique=False, nullable=False)
+    shipping_sender = product_name = db.Column(
+        db.String(80), unique=False, nullable=False)
+    shipping_receiver = product_name = db.Column(
+        db.String(80), unique=False, nullable=False)
 
-    def __repr__(self):
+    def repr(self):
         return "<Shipping %r>" % self.shipping_id
+
+
+# the class for Review objects
+class Review(db.Model):
+    review_id = db.Column(db.Integer, primary_key=True)
+    buyer_username = db.Column(db.String(80), unique=True, nullable=False)
+    seller_username = db.Column(db.String(80), unique=True, nullable=False)
+    comment = db.Column(db.String(120), unique=True, nullable=False)
+
+    def repr(self):
+        return '<Review %r>' % self.review_id
