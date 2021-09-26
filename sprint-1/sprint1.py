d0e62068ba0da8483580b84d7ee833a37ecf35e5
Yucan Li <18yl259@queensu.ca>
2021-09-26 17:12:04 -0400

    add shipping entry into sprint1.py

--- sprint-1/sprint1.py
+++ sprint-1/sprint1.py
@@ -27,3 +27,14 @@ class Product(db.Model):
 
     def __repr__(self):
         return "<Product %r>" % self.product_id
+
+# class for shipping after transaction
+class  shipping(db.Model):
+    shipping_id = db.Column(db.Integer, primary_key=True)
+    shipping_contact = db.Column(db.Integer, primary_key=True)
+    shipping_company = db.Column(db.String(80), unique=False, nullable=False)
+    shipping_sender = product_name = db.Column(db.String(80), unique=False, nullable=False)
+    shipping_receiver = product_name = db.Column(db.String(80), unique=False, nullable=False)
+
+    def __repr__(self):
+        return "<Shipping %r>" % self.shipping_id
