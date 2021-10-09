8f484acfba83132fd9ac9788098d5518c2111d29
YucanLi <1579193739@qq.com>
2021-10-09 02:12:31 -0400

    update user name and product profile

--- qbay/models.py
+++ qbay/models.py
@@ -1,6 +1,7 @@
 from qbay import app
 from flask_sqlalchemy import SQLAlchemy
 import re
+from datetime import datetime
 
 '''
 This file defines data models and related business logics
@@ -56,6 +57,26 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User %r>' % self.username
+
+
+# the class for product objects
+class Product(db.Model):
+    id = db.Column(
+        db.Integer, primary_key=True)
+    title = db.Column(
+        # the title of the product cannot be empty
+        db.String(80), unique=True, nullable=True)
+    description = db.Column(
+        db.String(2000), unique=False, nullable=False)
+    price = db.Column(
+        db.Integer, unique=False, nullable=False)
+    last_modified_date = db.Column(
+        db.Integer, unique=False, nullable=False)
+    owner_email = db.Column(
+        db.String(120), unique=False, nullable=False)
+
+    def __repr__(self):
+        return "<Product %r>" % self.title
 
 
 # create all tables
@@ -119,3 +140,89 @@ def login(email, password):
     if len(valids) != 1:
         return None
     return valids[0]
+
+
+# using email to find account
+def update_profile(email, name, shipping_address, postal_code):
+    '''
+    Update user profile
+      Parameters:
+        name (string):              user name
+        shipping_address (string):  shipping address
+        postal_code (string):       postal code
+      Returns:
+        Boolean value, true for successful update otherwise update fails
+    '''
+
+    # shipping address should not be empty or contain no special charactors
+    if (not(shipping_address) or
+            (shipping_address.isalnum() is False)):
+            return False
+    # Canadian zip code configuration
+    if not((len(re.findall(
+        r'[A-Z]{1}[0-9]{1}[A-Z]{1}\s*[0-9]{1}[A-Z]{1}[0-9]{1}',
+            postal_code.upper()))) == 1):
+        return False
+    # user name should not empty and not space at prefix or suffix
+    if (not(name) or len(name) >= 20 or len(name) <= 2 or
+        name[0] == " " or
+        name[-1] == " " or
+            not(all(i.isalnum() or i.isspace() for i in name))):
+        return False
+
+    user = User.query.filter_by(email=email).first()
+    user.name = name
+    user.postal_code = postal_code
+    user.shipping_address = shipping_address
+    db.session.commit()
+
+    return True
+
+
+def update_product(title, new_title, description, price):
+    '''
+    Update Product info
+      Parameters:
+        id (integer):           product id
+        title (string):         product name
+        description (string):   product description
+        price (integer):        product price
+      Returns:
+        Boolean value, true for successful update otherwise update fails
+    '''
+
+    # check if the title of the product is not alphanumeric-only,
+    # and space allowed only
+    if not(all(c.isalnum() or c.isspace() for c in new_title)):
+        return None
+    # check if prefix or suffix has space
+    if ((len(new_title) != 0) and
+        (new_title[0] == ' ') or (new_title[-1] == ' ')):
+        return None
+    # check if the title has more than 80 characters
+    if len(new_title) > 80:
+        return None
+    # check if the length of description is less than 20 characters
+    # or more than 2000 characters
+    if (len(description) < 20) | (len(description) > 2000):
+        return None
+    # check if the length of description is less than the length of title
+    if len(description) < len(new_title):
+        return None
+    # check if the price is less than 10 or more than 10000
+    if (price < 10) | (price > 10000):
+        return None
+
+    product = Product.query.filter_by(title=title).first()
+    # express last modified date in format of dd/mm/yy
+    product.last_modified_date = datetime.today().strftime('%Y-%m-%d')
+    product.title = new_title
+    product.description = description
+    # determine wheather new price is incremental than previous one
+    if (price < product.price):
+        return None
+    else:
+        product.price = price
+    db.session.commit()
+
+    return product
--- qbay_test/test_models.py
+++ qbay_test/test_models.py
@@ -1,5 +1,8 @@
 from qbay.models import register, login
-
+from typing import Counter
+from datetime import datetime
+from qbay.models import Product, User, update_product, update_profile
+from qbay.models import create_product
 
 def test_r1_7_user_register():
     '''
@@ -47,3 +50,131 @@ def test_r2_1_login():
 
     user = login('testXXX@test.com', "Hello123!!")
     assert user is None
+
+
+def test_r3_1_update_user_profile():
+    '''
+    Testing R2-1: A user is only able to update
+    his/her user name, shipping_address, and postal_code.
+    '''
+    assert register('USER0', '12345@qq.com', 'Hello123!!') is True
+    assert update_profile("12345@qq.com", 'Tom',
+                          '123UniversityAve', 'K7L 0Y3') is True
+
+
+def test_r3_2_update_user_profile():
+    '''
+    R3-2: Shipping_address should be non-empty,
+    alphanumeric-only, and no special characters such as !
+    '''
+    assert update_profile("12345@qq.com", 'Tom', '', 'K7L 0Y3') is False
+    assert update_profile(
+                        "12345@qq.com",
+                        'Tom',
+                        '123! University Ave',
+                        'K7L 0Y3') is False
+
+
+def test_r3_3_update_user_profile():
+    '''
+    R3-3: Postal code has to be a valid Canadian postal code
+    '''
+    assert update_profile(
+            "12345@qq.com",
+            'Tom',
+            '123UniversityAve',
+            'K7L 0Y3') is True
+    assert update_profile(
+        "12345@qq.com",
+        'Tom',
+        '123UniversityAve',
+        'K0Y3') is False
+
+
+def test_r3_4_update_user_profile():
+    '''
+    R3-4: User name follows the requirements above
+    '''
+    assert update_profile(
+        "12345@qq.com",
+        'm',
+        '123UniversityAve',
+        'K7L 0Y3') is False
+    assert update_profile(
+        "12345@qq.com",
+        'qwertyuiopasdfghjklz',
+        '123UniversityAve',
+        'K7L 0Y3') is False
+    assert update_profile(
+        "12345@qq.com",
+        '',
+        '123 University Ave',
+        'K7L 0Y3') is False
+    assert update_profile(
+        "12345@qq.com",
+        ' Tim ',
+        '123UniversityAve',
+        'K7L 0Y3') is False
+
+
+def test_r5_1_update_product():
+    '''
+    R5-1: One can update all attributes of the product,
+    except owner_email and last_modified_date
+    '''
+
+    product = create_product(
+        'apple',
+        'This is a description',
+        10, '2021-10-07',
+        'aa12a@queensu.ca')
+    assert update_product(
+        "apple",
+        "apple",
+        "an apple a day keeps doctor away",
+        30) is not None
+
+
+def test_r5_2_update_product():
+    '''
+    R5-2: Price can be only increased but cannot be decreased
+    '''
+
+    assert update_product(
+        "apple",
+        "apple",
+        "good for health good for health",
+        29) is None
+
+
+def test_r5_3_update_product():
+    '''
+    R5-3: last_modified_date should be updated when the
+    update operation is successful
+    '''
+    product = update_product(
+        "apple",
+        "apple",
+        "good for health good for health",
+        1000)
+    # check if the last_modified_date is corrected updated after update_product
+    assert product.last_modified_date == datetime.today().strftime('%Y-%m-%d')
+
+
+def test_r5_4_update_product():
+    '''
+    R5-4: When updating an attribute, one has to make sure
+    that it follows the same requirements as above
+    '''
+
+    assert update_product(
+        "apple",
+        "apple",
+        "good for health good for health",
+        2000) is not None
+
+    assert update_product(
+        "apple",
+        "apple ",
+        "good for health good for health",
+        1006) is None
