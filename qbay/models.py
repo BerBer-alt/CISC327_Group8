98caabe0bc7e24ec899ae71937586e5e970dd355
Irving <Irv1ngzhu@hotmail.com>
2021-10-09 14:19:03 -0400

    added check_email implementation, and added test model for registeration and login

--- qbay/models.py
+++ qbay/models.py
@@ -1,10 +1,64 @@
 from qbay import app
 from flask_sqlalchemy import SQLAlchemy
-
+import re
 
 '''
 This file defines data models and related business logics
 '''
+
+
+# this function check if the Email address inputed fits the format or not
+def check_Email(Input_str: str):
+    if Input_str == "":
+        return False
+    regex = r"(^[a-zA-Z0-9_.+-/!#$%&'*/=?^_`{|}~\" ]" \
+            r"+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)"
+    if re.search(regex, Input_str) is None:
+        return False
+    domain = Input_str[0:Input_str.index("@")]
+    if domain[0] == "." or domain[-1] == ".":
+        return False
+    if domain.find("\"") != -1:
+        fir_q = domain.find("\"")
+        sec_q = domain.find("\"", fir_q + 1)
+        if sec_q == -1:
+            return False
+        domain = domain[0:fir_q] + domain[sec_q + 1:]
+        if domain.find(".") == -1 and fir_q != 0 and sec_q != len(Input_str):
+            return False
+    if domain.find("..") != -1 or domain.find(" ") != -1:
+        return False
+
+    return True
+
+
+# this function check the string that input fits the requirnment or not
+def check_Password(Input_password: str):
+    if Input_password.__len__() < 6:
+        return False
+    Flag_upper = False
+    Flag_lower = False
+    Flag_Spec = False
+    for i in Input_password:
+        if i.isupper():
+            Flag_upper = True
+        elif i.islower():
+            Flag_lower = True
+        elif i in "!\"#$%&\'()*+,-./:;<=>?@[\]^_`{|}~":
+            Flag_Spec = True
+    return Flag_Spec and Flag_lower and Flag_upper
+
+
+# this function check the input string fits the user requirnment or not
+def check_Username(Input_Username: str):
+    if Input_Username.__len__() <= 2 or Input_Username.__len__() > 20:
+        return False
+    if Input_Username[0] == " " or Input_Username[-1] == " ":
+        return False
+    for i in Input_Username:
+        if not i.isalnum():
+            return False
+    return True
 
 
 db = SQLAlchemy(app)
@@ -18,6 +72,17 @@ class User(db.Model):
         primary_key=True)
     password = db.Column(
         db.String(120), nullable=False)
+    shipping_address = db.Column(
+        db.String(120), unique=False, nullable=True,
+        primary_key=False)
+    postal_code = db.Column(
+        db.String(120), unique=False, nullable=True,
+        primary_key=False)
+    balance = db.Column(
+        db.Integer,
+        unique=False,
+        nullable=True,
+        primary_key=False)
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -37,13 +102,29 @@ def register(name, email, password):
       Returns:
         True if registration succeeded otherwise False
     '''
+
+    if not check_Username(name):
+        return False
+    if not check_Email(email):
+        return False
+    if not check_Password(password):
+        return False
+
     # check if the email has been used:
     existed = User.query.filter_by(email=email).all()
     if len(existed) > 0:
         return False
 
     # create a new user
-    user = User(username=name, email=email, password=password)
+    user = User(
+        username=name,
+        email=email,
+        password=password,
+        # Shipping address is empty at the time of registration
+        shipping_address="",
+        # Postal code is empty at the time of registration
+        postal_code="",
+        balance=100)  # free $100 dollar signup bonus
     # add it to the current database session
     db.session.add(user)
     # actually save the user object
@@ -61,6 +142,11 @@ def login(email, password):
       Returns:
         The user object if login succeeded otherwise None
     '''
+
+    if not check_Email(email):
+        return None
+    if not check_Password(password):
+        return None
     valids = User.query.filter_by(email=email, password=password).all()
     if len(valids) != 1:
         return None
--- qbay_test/test_models.py
+++ qbay_test/test_models.py
@@ -1,27 +1,114 @@
 from qbay.models import register, login
 
 
-def test_r1_7_user_register():
+def test_r1_1_user_register():
     '''
-    Testing R1-7: If the email has been used, the operation failed.
+        Testing R1-2: A user is uniquely identified by his/her email address.
+        R1-7: If the email has been used, the operation failed.
     '''
 
-    assert register('u0', 'test0@test.com', '123456') is True
-    assert register('u0', 'test1@test.com', '123456') is True
-    assert register('u1', 'test0@test.com', '123456') is False
+    assert register('USER011', 'test011@test.com', 'Hello123!!') is True
+    assert register('123123', '', 'Hello123!!') is False
+    assert register('USER0', 'test1@test.com', '') is False
+
+
+def test_r1_2_user_register():
+    '''
+        Testing R1-2: A user is uniquely identified by his/her email address.
+        R1-7: If the email has been used, the operation failed.
+    '''
+
+    assert register('USER012', 'test012@test.com', 'Hello123!!') is True
+    assert register('USER012', 'test012@test.com', 'Hello123!!') is False
+    assert register('USER2', 'Abc.example.com', 'Hello123!!') is False
+
+
+def test_r1_3_user_register():
+    '''
+        Testing R1-1: Both the email and password cannot be empty.
+    '''
+
+    assert register('User01', 'user01@example.com', '') is False
+    assert register('User02', '', 'Abc123!') is False
+    assert register('User01', 'user01@example.com', 'Abc012.') is True
+
+
+def test_r1_4_user_register():
+    '''
+        Testing R1-3: The email has to follow addr-spec defined in RFC 5322
+    '''
+    assert register('User02',
+                    'i_like_underscore@but_its_not_'
+                    'allowed_in_this_part.example.com',
+                    'Abc012.') is False
+    assert register('User02', 'just\"not\"right@example.com',
+                    'Abc123!') is False
+    assert register('User02', 'A@b@c@example.com', 'Abc123!') is False
+    assert register('User02', '\"john..doe\"@example.org', 'Abc012!') is True
+    assert register('User03', 'x@example.com', 'Abc012.') is True
+    assert register('User04', 'user.name+tag+sorting@example.com',
+                    'Abc012.') is True
+
+
+def test_r1_5_user_register():
+    '''
+        Testing R1-4: Password has to meet the required complexity:
+        minimum length 6, at least one upper case, at least
+        one lower case, and at least one special character.
+    '''
+    assert register('User05', 'user05@hotmail.com', 'Abc0123?..') is True
+    assert register('User06', 'user05@hotmail.com', 'Abc0123') is False
+    assert register('User06', 'user05@hotmail.com',
+                    'abcdefgsadaxz./???!') is False
+    assert register('User06', 'user05@hotmail.com',
+                    'Ab2.') is False  # too short
+
+
+def test_r1_6_user_register():
+    '''
+        Testing R1-5: User name has to be non-empty, alphanumeric-only,
+        and space allowed only if it is not as
+        the prefix or suffix.
+    '''
+
+    assert register('', 'user07@hotmail.com', 'Ab2.asd') is False
+    assert register('sdf~!!@??d', 'user07@hotmail.com', 'Ab2.ssda?') is False
+    assert register(' dddsa123', 'user07@hotmail.com', 'Ab2.asd') is False
+    assert register('dddsa123 ', 'user07@hotmail.com', 'Ab2.asd') is False
+    assert register('User07', 'user07@hotmail.com', 'Ab2.asd') is True
+
+
+def test_r1_7_user_register():
+    '''
+        Testing R1-6: User name has to be longer than
+        2 characters and less than 20 characters.
+    '''
+    assert register('W', 'user08@hotmail.com', 'Ab2.asd') is False
+    assert register('abcasjudhaksdhaosdijhaolsijdlkasjdiajdliasdjalskdjilja',
+                    'user08@hotmail.com', 'Ab2.asd') is False
+    assert register('User08', 'user08@hotmail.com', 'Ab2.asd') is True
 
 
 def test_r2_1_login():
     '''
-    Testing R2-1: A user can log in using her/his email address 
+    Testing R2-1: A user can log in using her/his email address
       and the password.
-    (will be tested after the previous test, so we already have u0, 
-      u1 in database)
+    (will be tested after the previous test)
     '''
-
-    user = login('test0@test.com', 123456)
+    user = login('user01@example.com', 'Abc012.')
     assert user is not None
-    assert user.username == 'u0'
+    assert user.username == 'User01'
 
-    user = login('test0@test.com', 1234567)
+    user = login('user05@hotmail.com', 'Abc0123?..')
+    assert user is not None
+    assert user.username == 'User05'
+
+    user = login('user08@hotmail.com', 'Ab2.asd')
+    assert user is not None
+    assert user.username == 'User08'
+
+    user = login('tuser08@hotmail.com', 'ab2.asd')
+    assert user is None
+
+    user = login('test012344@test.com', '1234567')
     assert user is None
