f8970ae7a648f15ccd37f095dfa79a105bca5ae0
Wenran Hou <18wh10@queensu.ca>
2021-10-09 18:47:44 -0400

    Add create_product, class product and transaction. Add test part of create_product.

--- qbay/models.py
+++ qbay/models.py
@@ -88,6 +88,31 @@ class User(db.Model):
         return '<User %r>' % self.username
 
 
+# the class for transaction objects
+class Transaction(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_email = db.Column(db.String(120), unique=True, nullable=False)
+    product_id = db.Column(db.String(80), unique=True, nullable=False)
+    price = db.Column(db.Integer, unique=False, nullable=False)
+    date = db.Column(db.String(10), unique=False, nullable=False)
+
+    def __repr__(self):
+        return "<Transaction %r>" % self.id
+
+
+# the class for product objects
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80), unique=True, nullable=True)
+    description = db.Column(db.String(2000), unique=False, nullable=False)
+    price = db.Column(db.Integer, unique=False, nullable=False)
+    last_modified_date = db.Column(db.String(10), unique=False, nullable=False)
+    owner_email = db.Column(db.String(120), unique=False, nullable=False)
+
+    def __repr__(self):
+        return "<Product %r>" % self.id
+
+
 # create all tables
 db.create_all()
 
@@ -151,3 +176,62 @@ def login(email, password):
     if len(valids) != 1:
         return None
     return valids[0]
+
+
+def create_product(title, description, price, last_modified_date, owner_email):
+    """
+    Create a new product.
+      Parameters:
+        title:               title
+        description:         description
+        price:               price
+        last_modified_date:  last_modified_date
+        owner_email:         owner_email
+      Returns:
+        True if creation succeeded otherwise False.
+    """
+    # check if the title of the product is not alphanumeric-only,
+    # and space allowed only
+    if not(all(c.isalnum() or c.isspace() for c in title)):
+        return False
+    # check if prefix or suffix has space
+    if (len(title) != 0) and (title[0] == ' ') or (title[-1] == ' '):
+        return False
+    # check if the title has more than 80 characters
+    if len(title) > 80:
+        return False
+    # check if the length of description is less than 20 characters
+    # or more than 2000 characters
+    if (len(description) < 20) | (len(description) > 2000):
+        return False
+    # check if the length of description is less than the length of title
+    if len(description) < len(title):
+        return False
+    # check if the price is less than 10 or more than 10000
+    if (price < 10) | (price > 10000):
+        return False
+    # check if the last modified date is before 2021-01-02 or after 2025-01-02
+    if last_modified_date < '2021-01-02':
+        return False
+    if last_modified_date > '2025-01-02':
+        return False
+    # check if owner email is empty,
+    # and the owner does not exist in the database
+    if owner_email == '':
+        return False
+    # check if the product has the same title
+    existed = Product.query.filter_by(title=title).all()
+    if len(existed) > 0:
+        return False
+
+    # create a new product
+    product = Product(id=db.session.query(Product).count() + 1, title=title,
+                      description=description, price=price,
+                      last_modified_date=last_modified_date,
+                      owner_email=owner_email)
+    # add it to the current database session
+    db.session.add(product)
+    # actually save the product object
+    db.session.commit()
+
+    return True
--- qbay_test/test_models.py
+++ qbay_test/test_models.py
@@ -1,4 +1,4 @@
-from qbay.models import register, login
+from qbay.models import register, login, create_product
 
 
 def test_r1_1_user_register():
@@ -115,3 +115,158 @@ def test_r2_1_login():
 
     user = login('test012344@test.com', '1234567')
     assert user is None
+
+
+def test_r4_1_create_product():
+    '''
+    Testing R4-1: The title of the product has to be alphanumeric-only, and
+    space allowed only if it is not as prefix and suffix.
+    '''
+
+    assert create_product('test 1', 'This is a description', 100,
+                          '2021-10-07', 'aa12a@queensu.ca') is True
+    assert create_product(' test 2', 'This is a description', 100,
+                          '2021-10-07', 'aa12a@queensu.ca') is False
+    assert create_product('test 3 ', 'This is a description', 100,
+                          '2021-10-07', 'aa12a@queensu.ca') is False
+
+
+def test_r4_2_create_product():
+    '''
+    Testing R4-2: The title of the product is no longer than 80 characters.
+    '''
+
+    assert create_product('test 4', 'This is a description', 100,
+                          '2021-10-07', 'aa12a@queensu.ca') is True
+    assert create_product('test 5 test 5 test 5 test 5 test 5 test 5 test 5'
+                          ' test 5 test 5 test 5 test 5 test 5',
+                          'This is a description', 100, '2021-10-07',
+                          'aa12a@queensu.ca') is False
+
+
+def test_r4_3_create_product():
+    '''
+    Testing R4-3: The description of the product can be arbitrary characters,
+    with a minimum length of 20 characters and a maximum of 2000 characters.
+    '''
+
+    assert create_product('test 6', 'This is a description', 100, '2021-10-07',
+                          'aa12a@queensu.ca') is True
+    assert create_product('test 7', 'Description', 100, '2021-10-07',
+                          'aa12a@queensu.ca') is False
+    assert create_product('test 8', '¡Sí, es verdad! Por eso enseño más '
+                          'cursos. Oye… ¿qué hora es?', 100, '2021-10-07',
+                          'aa12a@queensu.ca') is True
+    assert create_product('test 9', 'The work "done" by the force is the '
+                          'scalar product of the force vector and the '
+                          'displacement vector of the object. We say that the '
+                          'force "does work" if it is exerted while the '
+                          'object moves (has a isplacement vector) and in '
+                          'such a way that the scalar product of the force '
+                          'and displacement vectors is non-zero. A force '
+                          'that is perpendicular to the displacement vector '
+                          'of an object does no work (since the scalar '
+                          'product of two perpendicular vectors is zero). '
+                          'A force exerted in the same direction as the '
+                          'isplacement will do positive work (cos positive), '
+                          'and a force in the opposite direction of the '
+                          'displacement will do negative work(cos negative). '
+                          'As we will see, positive work corresponds to '
+                          'increasing the speed of theobject, whereas '
+                          'negative work corresponds to decreasing its '
+                          'speed. No work corresponds to no change in speed '
+                          '(but could corresponds to a change in velocity). '
+                          'You may be tempted to ask, "Why work? Why not '
+                          'something else? Why that scalar product in '
+                          'particular? How could we possibly have thought of '
+                          'that?". In general, it seems arbitrary that we '
+                          'introduce the quantity "work" and then find that '
+                          'it leads to a convenient way of building models. '
+                          'However, we did not just pull this quantity out of '
+                          'thin air! Many theorists, over many years, tried '
+                          'all sorts of quantities and ways to rephrase '
+                          'Newton’s Theory that were not helpful. The '
+                          'quantities that make it into textbooks are the '
+                          'ones that turned out to be useful. You should also '
+                          'keep in mind that, just like force, work is a '
+                          '"made-up" mathematical tool that is helpful in '
+                          'describing the world around us. There is no such '
+                          'thing as work or energy; they are just useful '
+                          'mathematical tools. Work involves vectors, so we '
+                          'can first examine the concept in one dimension, '
+                          'before extending this to two and three dimensions. '
+                          'We can choose x as the coordinate in one '
+                          'dimension, so that all vectors only have an x '
+                          'component. We can write a force vector as '
+                          '~F = F ˆx, where F is the x component of the '
+                          'force (which could be positive or negative). A '
+                          'displacement vector can be written as ~d = dˆx, '
+                          'where again, d is the x component of the '
+                          'displacement, and can be positive or negative. '
+                          'In one dimension, work is thus:', 100, '2021-10-07',
+                          'aa12a@queensu.ca') is False
+
+
+def test_r4_4_create_product():
+    '''
+    Testing R4-4: Description has to be longer than the product's title.
+    '''
+
+    assert create_product('test 10', 'This is a description', 100,
+                          '2021-10-07', 'aa12a@queensu.ca') is True
+    assert create_product('test 11', 'Test', 100, '2021-10-07',
+                          'aa12a@queensu.ca') is False
+
+
+def test_r4_5_create_product():
+    '''
+    Testing R4-5: Price has to be of range [10, 10000].
+    '''
+
+    assert create_product('test 12', 'This is a description', 100,
+                          '2021-10-07', 'aa12a@queensu.ca') is True
+    assert create_product('test 13', 'This is a description', 5, '2021-10-07',
+                          'aa12a@queensu.ca') is False
+    assert create_product('test 14', 'This is a description', 10005,
+                          '2021-10-07', 'aa12a@queensu.ca') is False
+
+
+def test_r4_6_create_product():
+    '''
+    Testing R4-6: last_modified_date must be after 2021-01-02
+    and before 2025-01-02.
+    '''
+
+    assert create_product('test 15', 'This is a description', 100,
+                          '2021-10-07', 'aa12a@queensu.ca') is True
+    assert create_product('test 16', 'This is a description', 100,
+                          '2020-10-07', 'aa12a@queensu.ca') is False
+    assert create_product('test 17', 'This is a description', 100,
+                          '2026-10-07', 'aa12a@queensu.ca') is False
+
+
+def test_r4_7_create_product():
+    '''
+    Testing R4-7: owner_email cannot be empty. The owner of the corresponding
+    product must exist in the database.
+    '''
+
+    assert create_product('test 18', 'This is a description', 100,
+                          '2021-10-07', 'aa12a@queensu.ca') is True
+    assert create_product('test 19', 'This is a description', 100,
+                          '2021-10-07', '') is False
+    assert create_product('test 20', 'This is a description', 100,
+                          '2021-10-07', 'bb12b@queensu.ca') is True
+
+
+def test_r4_8_create_product():
+    '''
+    Testing R4-8: A user cannot create products that have the same title.
+    '''
+
+    assert create_product('test 21', 'This is a description', 100,
+                          '2021-10-07', 'aa12a@queensu.ca') is True
+    assert create_product('test 21', 'This is a description', 100,
+                          '2021-10-07', 'aa12a@queensu.ca') is False
+    assert create_product('test 18', 'This is a description', 100,
+                          '2021-10-07', 'aa12a@queensu.ca') is False
