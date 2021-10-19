91b2d4ef1c4e554c9b266eced55b1d0ff6f4c8c7
Irving <Irv1ngzhu@hotmail.com>
2021-10-18 21:46:31 -0400

    updated _main_.py and models.py

--- qbay/__main__.py
+++ qbay/__main__.py
@@ -1,2 +1,38 @@
 from qbay import *
-from qbay.models import *
+from qbay.cli import login_page, register_page, profile_update_page, product_update_page, product_creation_page
+
+
+def main():
+    while True:
+        selection = input(
+            'Welcome. Please type 1 to login. Or type 2 register.')
+        selection = selection.strip()
+        if selection == '1':
+            user = login_page()
+            if user:
+                print(f'welcome {user.username}')
+                while True:
+                    selection = input(
+                        "Please type 1 to update your profile."
+                        "Type 2 to create product."
+                        "Type 3 to update product."
+                        "type something else to exit.")
+                    selection = selection.strip()
+                    if selection == '1':
+                        profile_update = profile_update_page()
+                    elif selection == '2':
+                        product_creation_page()
+                    elif selection == '3':
+                        product_update_page()
+                    else:
+                        break
+            else:
+                print('login failed')
+        elif selection == '2':
+            register_page()
+        else:
+            break
+
+
+if __name__ == '__main__':
+    main()
--- qbay/models.py
+++ qbay/models.py
@@ -309,7 +309,7 @@ def update_profile(email, name, shipping_address, postal_code):
         Boolean value, true for successful update otherwise update fails
     '''
 
-    # shipping address should not be empty or contain no special charactors
+    # shipping address should not be empty or contain no special characters
     if (not(shipping_address) or
             (shipping_address.isalnum() is False)):
         return False
@@ -335,7 +335,7 @@ def update_profile(email, name, shipping_address, postal_code):
     return True
 
 
-# using title to find existed product and update prodict informationr
+# using title to find existed product and update product information
 def update_product(title, new_title, description, price):
     '''
     Update Product info
@@ -377,8 +377,8 @@ def update_product(title, new_title, description, price):
     product.last_modified_date = datetime.today().strftime('%Y-%m-%d')
     product.title = new_title
     product.description = description
-    # determine wheather new price is incremental than previous one
-    if (price < product.price):
+    # determine whether new price is incremental than previous one
+    if price < product.price:
         return None
     else:
         product.price = price
