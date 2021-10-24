394d37fe8f284a8e33804547cf20f365d6dfa249
Wenran Hou <18wh10@queensu.ca>
2021-10-24 15:28:02 -0400

    Add more comments.

--- qbay/__main__.py
+++ qbay/__main__.py
@@ -3,38 +3,51 @@ from qbay.cli import login_page, register_page, profile_update_page
 from qbay.cli import product_update_page, product_creation_page
 
 
+# the function of Frontend CLI
 def main():
+    # use 'while true' to repeat actions of login or register
     while True:
+        # Receive the value of action
         selection = input(
             'Welcome. Please type 1 to login. Or type 2 register.')
         selection = selection.strip()
+        # The login part
         if selection == '1':
             # login situation
             user = login_page()
             if user:
+                # Return the result of login successfully
                 print(f'Welcome back {user.username}')
+                # Repeat actions of updating profile,
+                # creating product and updating product.
                 while True:
+                    # Select one action
                     selection = input(
                         "Please type 1 to update your profile.\n"
                         "Type 2 to create product.\n"
                         "Type 3 to update product.\n"
                         "Type something else to exit: ")
                     selection = selection.strip()
+                    # Update profile part
                     if selection == '1':
                         profile_update_page(user.email)
+                    # Create product part
                     elif selection == '2':
                         product_creation_page()
+                    # Update product part
                     elif selection == '3':
                         product_update_page()
+                    # Log out
                     else:
                         break
+            # Login unsuccessfully
             else:
                 print('login failed')
+        # register situation
         elif selection == '2':
-            # register situation
             register_page()
+        # press other character to stop the program
         else:
-            # press other character to stop the program
             break
 
 
--- qbay/cli.py
+++ qbay/cli.py
@@ -2,80 +2,104 @@ from qbay.models import login, register, create_product, update_profile, \
     update_product
 
 
+# The login part
 def login_page():
     '''
     User login page
       Parameters: Null
       Returns: Null
     '''
+    # Read the values of email and password
     email = input('Please input email: ')
     password = input('Please input password:')
+    # Check the correctness of email and password
     return login(email, password)
 
 
+# The register part
 def register_page():
     '''
     Registration page
       Parameters: Null
       Returns: Null
     '''
+    # Read the values of name, email, password and confirming password
     name = input('Please input user name: ')
     email = input('Please input email: ')
     password = input('Please input password: ')
     password_twice = input('Please input the password again: ')
+    # Compare password with confirming password
     if password != password_twice:
         print('password entered not the same')
+    # Register a new user
     elif register(name, email, password):
         print('registration succeeded')
+    # Unsuccessful registration
     else:
         print('registration failed.')
 
 
+# The update_profile part
 def profile_update_page(email):
     '''
     Update profile page
       Parameters: identification email
       Returns: Null
     '''
+    # Read values of name, shipping_address and postal_code
     name = input("Please change user's name: ")
     shipping_address = input("Please change shipping address: ")
     postal_code = input("Please Change postal code: ")
+    # Update the profile
     if update_profile(email, name, shipping_address, postal_code):
         print("Profile Updated Successfully!")
+    # Update unsuccessfully
     else:
         print("Profile Update Failed...")
 
 
+# The create_product part
 def product_creation_page():
     '''
     Create product page
       Parameters: Null
       Returns: Null
     '''
+    # Read values of title, description, price, last modified date
+    # and owner email
     title = input("please input title: ")
     description = input("please input description: ")
     price = int(input("please input price: "))
     last_mod_date = input("please input last modified date")
     owner_email = input("please input owner's email: ")
+    # Create a new product
     res = create_product(title, description, price, last_mod_date, owner_email)
+    # Create unsuccessfully
     if res is False:
         print("Product Creation failed.")
+    # Create successfully
     else:
         print("Creation succeed. ")
 
 
+# The update_product part
 def product_update_page():
     '''
     Update product page
       Parameters: Null
       Returns: Null
     '''
+    # Read the values of title(use this to confirm the product), new title,
+    # description and price
     title = input("please input former product name: ")
     new_tit = input("please input new product title name: ")
     des = input("please input new product description: ")
     price = int(input("please input new price: "))
+    # Update the product
     res = update_product(title, new_tit, des, price)
+    # Update unsuccessfully
     if res is None:
         print("update failed")
+    # Update successfully
     else:
         print("update succeed")
